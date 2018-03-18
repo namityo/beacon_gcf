18f03e288d3837720207b724a480ffd938703073
Namiki Kazuma <namityo@hotmail.com>
2018-03-18 14:33:48 +0900

    functionでdatastoreに書き込みえるようにする

--- functions/pubsub/index.js
+++ functions/pubsub/index.js
@@ -1,7 +1,37 @@
+const beaconlog = require('../../lib/datastore/beaconlog')
+
+
 module.exports = (event, callback) => {
     const pubsubMessage = event.data
 
-    console.log(pubsubMessage)
+    // データチェック
+    if (checkData(pubsubMessage)) {
 
-    callback()
-}
+        // 登録
+        beaconlog.add(pubsubMessage, (err) => {
+            if (err) console.log(err); 
+        });
+    } else {
+        console.log("Invalid data was entered.");
+    }
+
+    callback();
+};
+
+
+/**
+ * データチェック用関数
+ * @param {*} data 
+ */
+function checkData(data) {
+    
+    // 日付が設定されていなければ現在時刻を設定
+    if (data.created == null) data.created = new Date();
+
+    return (
+        data.detector != null &&
+        data.uuid != null &&
+        data.major != null &&
+        data.minor != null
+    );
+};
